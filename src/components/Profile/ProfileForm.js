3a6b7ca6f2e8a48c05592d373829bcc191326ea2
Vitor Jaguat <vitorbutkus@gmail.com>
2022-12-28 17:54:21 -0300

    begin adding Change Password feature to ProfileForm.js

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,34 @@
+import { useRef } from 'react';
+import API_key from '../../env';
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    //add validation
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_key}`
+    );
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={enteredNewPassword} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
